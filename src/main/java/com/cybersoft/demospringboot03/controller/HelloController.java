8b2cfb590ed144b0d57c01394aa05dd922cfb3fa
Bình Minh <105420232+Bminh2312@users.noreply.github.com>
2023-10-31 21:07:40 +0700

    first commit

---
+++ src/main/java/com/cybersoft/demospringboot03/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.cybersoft.demospringboot03.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(){
+
+        return "Hello Spring boot";
+    }
+
+    @GetMapping("/cybersoft")
+    public String helloCybersoft(){
+        return "Hello Spring Cybersoft";
+    }
+}
